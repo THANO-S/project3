6135278ddd2dbcbefc418d979110ad773f98759d
Christopher Pomposo <chris2017pg@gmail.com>
2025-02-05 16:51:07 -0600

    first commit of initial state of files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+<template>
+  
+  <main class=" app">
+
+    <section class="greeting">
+      <h2 class="title">
+        <input type="text" placeholder="Enter Your Name" v-model="name">'s Contact List
+      </h2>
+    </section>
+
+
+    <section class="create-contactlist">
+
+    </section>
+
+
+    <section class="contactlist">
+
+    </section>
+
+
+
+
+  </main>
+
+
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const username=ref('')
+
+
+</script>
+
