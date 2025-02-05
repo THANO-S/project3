35dbc46fd4c024d32f1012f1196d34ff00aa1ac5
Christopher Pomposo <chris2017pg@gmail.com>
2025-02-05 17:50:06 -0600

    completed 2/3 of application.  Greeting and adding to list complete.

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,12 @@
 
 
     <section class="create-contactlist">
-
+      <h3>Create Your Contact List</h3>
+      <form @submit.prevent="addC">
+        <input type="text" placeholder="Enter Contact Name" v-model="newConName">
+        <input type="text" placeholder="Enter Contact Number" v-model="newConNum">
+        <input type="submit" value="Add Contact"/>
+      </form>
     </section>
 
 
@@ -30,8 +35,26 @@
 <script setup>
 import {ref} from 'vue'
 
-const username=ref('')
+const name =ref('');
+const newConName=ref('');
+const newConNum=ref('');
+//array to store contacts
+const contacts=ref([]);
 
+const addC = () => {
+  if(newConName.value.trim() === '' || newConNum.value.trim() ===''){
+    alert('Please provide a name and phone number.')
+    return
+  }
+
+  contacts.value.push({
+    name: newConName.value,
+    num: newConNum.value,
+  })
+
+  newConName.value = ''
+  newConNum.value = ''
+}
 
 </script>
 
